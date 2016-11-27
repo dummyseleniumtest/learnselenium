ad013aea9188e062b2eb2b5d5e3ffe49762effc0
naveen <naveensharma028@yahoo.com>
2016-11-27 22:50:28 +0530

    Test Java Using GitHub

---
+++ src/main/java/githubpracticeJava/HelloJava.java
@@ -0,0 +1,14 @@
+package githubpracticeJava;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println("Hello Java");
+
+	}
+
+}
